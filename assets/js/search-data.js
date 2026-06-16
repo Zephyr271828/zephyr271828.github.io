6d748ca3fc32e148d88321cb6ed48b91755617ea
Zephyr271828 <yx3038@nyu.edu>
2026-06-16 07:33:00 +0000

    Deploying to gh-pages from @ Zephyr271828/zephyr271828.github.io@3a9389dbae8e72871e5db1f32c3d48d3ee48c17a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -132,6 +132,13 @@ ninja.data = [{
           window.open("", "_blank");
         },
       },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/Zephyr271828", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
